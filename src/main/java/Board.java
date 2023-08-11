117cb5824f8870423a2ee2708c2c00af63060c55
Bartłomiej Konecki <bartekkonecki97@gmail.com>
2023-08-11 08:22:15 +0200

    Update Board.java
    Created function makeMove and isMoveLegal

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,4 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Board {
+    List<Character> columns = Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H');
+    List<Integer> rows = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
     char[][] board =
     {
         {
@@ -28,9 +33,33 @@ public class Board {
     };
     private String whitePlayer = "Player 1";
     private String blackPlayer = "Player 2";
+    private String currentPlayer = "white";
 
     public Board(String whitePlayer, String blackPlayer){
         this.blackPlayer = blackPlayer;
         this.whitePlayer = whitePlayer;
+    }
+
+    /**
+     * Function attempts to make a move on the board
+     * @param move, move written in chess notation
+     */
+    public void makeMove(String move){
+        char movingPiece;
+        if(columns.contains(move.charAt(0))){
+            movingPiece = 'P';
+        } else {
+            movingPiece = move.charAt(0);
+        }
+    }
+
+    /**
+     * Function checks if the move is legal
+     * @param move, move to be tested
+     * @param currentPlayer, player currently playing
+     * @return true if move is legal, false if move is illegal
+     */
+    private boolean isMoveLegal(String move, String currentPlayer){
+        return false;
     }
 }
